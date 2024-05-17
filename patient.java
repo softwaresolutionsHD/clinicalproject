a5163d14c097496787770f560d7275b7a6be34f2
softwaresurgery <softwaresurgery@gmail.com>
2024-05-17 09:17:24 -0700

    first version of paitient code

---
+++ patient.java
@@ -0,0 +1,15 @@
+
+package exceptionhandling;
+
+public class ExceptionTest {
+    public static void main(String[] args) {
+        int num []={1,2,3,4};
+        try{
+            System.out.println("Access Element 5:"+num[4]);
+        }
+        catch(Exception e ){
+            System.out.println("Exception Thrown:"+e);   
+        }
+            System.out.println("out of try-catch block!");
+        }
+    }
